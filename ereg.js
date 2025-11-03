4d8da46eb12ce2e4aa1ee9ec3cd4bb574da81d70
dubromelle <jerome.dubromelle@ac-amiens.fr>
2025-11-03 10:19:44 +0100

    simplification code

--- ereg.js
+++ ereg.js
@@ -4,5 +4,4 @@ function testRegex(pattern, input)
     return regex.test(input); 
 } 
 
-//tester si la chaîne contient «atari 2600» avec un espace qui sépare les 2 mots
-console.log(testRegex("atari\\s2600", "console atari 2600")); // true
+
